0a9c8aeaafef5f7ef97cca3a4723a5e56c4622f8
Aaditya jha <aadityajha2000@gmail.com>
2020-11-17 17:09:53 +0545

    Deployement configured to serve netlify app

--- routes/cors.js
+++ routes/cors.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://DESKTOP-7TNJILS:3001', 'http://localhost:3001'];
+const whitelist = [
+  "http://localhost:3000",
+  "https://priceless-jepsen-13b672.netlify.app",
+  "https://localhost:3443",
+  "http://DESKTOP-7TNJILS:3001",
+  "http://localhost:3001",
+];
 var corsOptionsDelegate = (req, callback) => {
-    var corsOptions;
+  var corsOptions;
 
-    if(whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true};
-    }
-    else{
-        corsOptions = {origin: false};
-    }
-    callback(null, corsOptions);
-}
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
 
 exports.cors = cors();
 exports.corsWithOptions = cors(corsOptionsDelegate);
